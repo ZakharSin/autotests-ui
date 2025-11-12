4c9186167a7031d014a8d7f0238c254884c35a17
ZakharSin <zakharsinitsa13@gmail.com>
2025-11-13 02:12:42 +0700

    Практика работы с pytest фикстурами v2

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,41 +2,40 @@ import pytest
 from playwright.sync_api import Playwright, sync_playwright, Page
 
 @pytest.fixture(scope="session")
-def initialize_browser_state():
-    with sync_playwright() as playwright:
-        # Открываем браузер и создаем новую страницу
-        browser = playwright.chromium.launch(headless=False)
-        context = browser.new_context()
-        # Открываем новую страницу в рамках контекста
-        page = context.new_page()
+def initialize_browser_state(playwright: Playwright) -> Page:
+    # Открываем браузер и создаем новую страницу
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    # Открываем новую страницу в рамках контекста
+    page = context.new_page()
 
-        # Переходим на страницу входа
-        page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+    # Переходим на страницу входа
+    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
 
-        # Заполняем поле email
-        email_input = page.get_by_test_id('registration-form-email-input').locator('input')
-        email_input.fill("user.name@gmail.com")
+    # Заполняем поле email
+    email_input = page.get_by_test_id('registration-form-email-input').locator('input')
+    email_input.fill("user.name@gmail.com")
 
-        # Заполняем поле username
-        username_input = page.get_by_test_id('registration-form-username-input').locator('input')
-        username_input.fill("username")
+    # Заполняем поле username
+    username_input = page.get_by_test_id('registration-form-username-input').locator('input')
+    username_input.fill("username")
 
-        # Заполняем поле password
-        password_input = page.get_by_test_id('registration-form-password-input').locator('input')
-        password_input.fill("password")
+    # Заполняем поле password
+    password_input = page.get_by_test_id('registration-form-password-input').locator('input')
+    password_input.fill("password")
 
-        # Нажимаем на кнопку registration
-        registration_button = page.get_by_test_id('registration-page-registration-button')
-        registration_button.click()
+    # Нажимаем на кнопку registration
+    registration_button = page.get_by_test_id('registration-page-registration-button')
+    registration_button.click()
 
-        context.storage_state(path='browser-state.json')
-        browser.close()
+    context.storage_state(path='browser-state.json')
+    browser.close()
 
-@pytest.fixture(scope="function")
+@pytest.fixture
 def chromium_page_with_state(initialize_browser_state, playwright: Playwright) -> Page:
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context(storage_state='browser-state.json')
-    Page = context.new_page()
-    Page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
-    yield Page
+    page = context.new_page()
+    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
+    yield page
     browser.close()
--- tests/test_courses.py
+++ tests/test_courses.py
@@ -7,18 +7,18 @@ from playwright.sync_api import Playwright, Page, sync_playwright, expect
 @pytest.mark.regression
 def test_empty_courses_list(chromium_page_with_state: Page):
 
-        # Проверка наличия и текста заголовка "Courses"
-        courses_text =  chromium_page_with_state.get_by_test_id('courses-list-toolbar-title-text')
-        expect(courses_text).to_be_visible()
+    # Проверка наличия и текста заголовка "Courses"
+    courses_text =  chromium_page_with_state.get_by_test_id('courses-list-toolbar-title-text')
+    expect(courses_text).to_be_visible()
 
-        # Проверка наличия иконки
-        icon = chromium_page_with_state.get_by_test_id('courses-list-empty-view-icon')
-        expect(icon).to_be_visible()
+    # Проверка наличия иконки
+    icon = chromium_page_with_state.get_by_test_id('courses-list-empty-view-icon')
+    expect(icon).to_be_visible()
 
-        # Проверка наличия и текста "There is no results"
-        no_results_text = chromium_page_with_state.get_by_test_id('courses-list-empty-view-title-text')
-        expect(no_results_text).to_be_visible()
+    # Проверка наличия и текста "There is no results"
+    no_results_text = chromium_page_with_state.get_by_test_id('courses-list-empty-view-title-text')
+    expect(no_results_text).to_be_visible()
 
-        # Проверка наличия и текста "Results from the load test pipeline will be displayed here"
-        results_text = chromium_page_with_state.get_by_test_id('courses-list-empty-view-description-text')
-        expect(results_text).to_be_visible()
+    # Проверка наличия и текста "Results from the load test pipeline will be displayed here"
+    results_text = chromium_page_with_state.get_by_test_id('courses-list-empty-view-description-text')
+    expect(results_text).to_be_visible()
