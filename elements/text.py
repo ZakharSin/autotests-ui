01be2c7c05bd76d29f7fddea737163626f6a66fc
ZakharSin <ZakharSin@users.noreply.github.com>
2026-05-01 14:29:45 +0000

    deploy: 43db10f3374abf530e7c04832da5ae1dd174bb76

--- elements/text.py
+++
@@ -1,8 +0,0 @@
-from elements.base_element import BaseElement
-
-
-class Text(BaseElement):
-    def to_have_text(self, param):
-        @property
-        def type_of(self) -> str:
-            return "text"
